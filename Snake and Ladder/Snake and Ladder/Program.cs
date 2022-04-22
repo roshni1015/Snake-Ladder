19117e2feb43da70dfd38adc56ff25a7c95315d9
roshni1015 <roshniadatrao1110@gmail.com>
2022-04-23 01:30:38 +0530

    Rolling Dice

--- Snake and Ladder/Snake and Ladder/Program.cs
+++ Snake and Ladder/Snake and Ladder/Program.cs
@@ -2,4 +2,8 @@
 Console.WriteLine("Welcome to Snake and Ladder");
 int playerPosition = 0;
 Console.WriteLine(playerPosition);
+Random random = new Random();
+int rollDie = random.Next(1, 7);
+Console.WriteLine(rollDie);
+
 
